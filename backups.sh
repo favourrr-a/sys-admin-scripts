d5f99275f232dfeedf975383528cf1c8a6b1d4a6
gene-999 <eugenebaidoo5@gmail.com>
2024-07-07 00:15:00 +0000

    commit for backup script

---
+++ backups.sh
@@ -0,0 +1,33 @@
+#! bin/bash
+
+if [ $# -ne 2 ]; then
+    echo "Usage: backup.sh <source_directory> <target_directory>"
+    exit 1
+fi
+
+if ! command -v rsync &> /dev/null; then
+    echo "rsync command not found"
+    exit 2
+fi
+
+
+current_date=$(date +%Y-%m-%d)
+
+backup_file="$2/backup-$current_date.tar.gz"
+backup_log="$2/backup-$current_date.log"
+
+if [ ! -d "$2" ]; then
+    mkdir -p "$2"
+fi
+
+rsync_options="-avb --backup-dir $2/$current_date --delete "
+
+$(which rsync) $rsync_options $1 $2/current >> $backup_log
+
+tar -czf $backup_file $1 
+
+if [ $? -eq 0 ]; then
+    echo "Backup of $1 completed successfully"
+else
+    echo "Backup of $1 failed"
+fi
